f7106468f46f04e30de38306593e751e3bd7b25a
Scott Morris <scott.moris@gmail.com>
2015-02-11 22:21:55 -0500

    Added database initializer for test data

---
+++ KVS/Dal/DatabaseInitializer.cs
@@ -0,0 +1,22 @@
+﻿using KVS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KVS.Dal
+{
+    public class DatabaseInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<ApplicationDbContext>
+    {
+        protected override void Seed(ApplicationDbContext context)
+        {
+            //base.Seed(context);
+            var clients = new List<Client>
+            {
+                new Client {Created = DateTime.Now, EmailAddress = "a@b.com", Name = "John Smith"}
+            };
+            clients.ForEach(c => context.Clients.Add(c));
+            context.SaveChanges();
+        }
+    }
+}
